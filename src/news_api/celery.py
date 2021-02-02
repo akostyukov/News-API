227600ee71a6b5b4545da65d116999a123dd3604
Artem Kostyukov <apkostyukov@gmail.com>
2021-02-02 11:23:52 +0300

    celery edits

--- src/news_api/celery.py
+++ src/news_api/celery.py
@@ -1,12 +1,10 @@
-from __future__ import absolute_import
-
 import os
 
 from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "news_api.settings")
 
-app = Celery("app", broker="amqp://rabbitmq:rabbitmq@rabbitmq:5672")
+app = Celery("app", broker="amqp://rabbitmq:rabbitmq@rabbitmq:5672", include=['news_api.consumers'])
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
